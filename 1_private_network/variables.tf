50270a5647d18365f1f56f698e15d2f0b5f43373
Gang Chen <gangchen@us.ibm.com>
2019-10-14 11:31:02 -0500

    develop the terraform scripts to provision AWS VPC and private subnets
    Signed-off-by: Gang Chen <gangchen@us.ibm.com>

---
+++ 1_private_network/variables.tf
@@ -0,0 +1,30 @@
+####### AWS Access and Region Details #############################
+variable "aws_region" {
+  default  = "us-east-2"
+  description = "One of us-east-2, us-east-1, us-west-1, us-west-2, ap-south-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, ap-northeast-1, us-west-2, eu-central-1, eu-west-1, eu-west-2, sa-east-1"
+}
+
+variable "aws_azs" {
+  type  = "list"
+  description = "The availability zone letter appendix you want to deploy to in the selected region "
+  default = ["a", "b", "c"]
+}
+
+variable "default_tags" {
+  default = {}
+}
+
+variable "infrastructure_id" {
+  default = ""
+}
+
+
+variable "clustername" { default = "ocp4" }
+variable "vpc_cidr" { default = "10.10.0.0/16" }
+
+# Subnet Details
+variable "vpc_private_subnet_cidrs" {
+  description = "List of subnet CIDRs"
+  type        = "list"
+  default     = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24" ]
+}
---
+++ 1_private_network/vpc.tf
@@ -0,0 +1,14 @@
+# create a vpc
+resource "aws_vpc" "ocp_vpc" {
+  cidr_block = "${ var.vpc_cidr }"
+  
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = "${merge(
+    var.default_tags, 
+    map(
+      "Name", "${local.infrastructure_id}-pri",
+    )
+  )}"
+}
