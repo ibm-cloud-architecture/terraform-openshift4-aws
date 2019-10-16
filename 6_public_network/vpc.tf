5c5e2197a47a7873e4382066f0d7ac3a5d279206
Gang Chen <gangchen@us.ibm.com>
2019-10-16 17:54:29 -0500

    Notes to update the public VPC code to support bastion host and private zone routing
    Signed-off-by: Gang Chen <gangchen@us.ibm.com>

--- 6_public_network/vpc.tf
+++ 6_public_network/vpc.tf
@@ -1,4 +1,8 @@
 # create a vpc
+# In real world, we wouldn't create the VPC, customer will have the VPC
+# And the terraform bastion host shold run in that VPC.
+# So need to change this code to use existing VPC
+# And we might also need to attach this public to the private Route53 zone as well, maybe
 resource "aws_vpc" "ocp_public_vpc" {
   cidr_block = "${ var.vpc_cidr }"
   
