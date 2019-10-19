d90a6820e31791a645c564d6ec159e2567e66d76
EC2 Default User <ec2-user@ip-172-16-20-55.us-east-2.compute.internal>
2019-10-19 19:13:34 +0000

    Before mods for final push

--- 8_bootstrap/rhcos.tf
+++ 8_bootstrap/rhcos.tf
@@ -3,30 +3,9 @@ data "aws_ami" "rhcos" {
 
     owners = ["531415883065"]
 
-    name_regex = "^rhcos-410\\.\\d{1}\\..*-hvm"
-    filter {
-        name = "name"
-        values = ["rhcos-4*-hvm"]
-    }
-
     filter {
         name = "image-id"
         values = ["${var.ami}"]
-    }
-
-    filter {
-        name   = "architecture"
-        values = ["x86_64"]
-    }
-
-    filter {
-        name   = "image-type"
-        values = ["machine"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
     }
 
 }
