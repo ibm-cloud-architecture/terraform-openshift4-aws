631aea3e7d801a44bbabb70d1688596c6d36dfb0
Budi Darmawan <vbudi@us.ibm.com>
2019-10-30 22:01:47 -0500

    update merge public subnets

--- 1_private_network/output.tf
+++ 1_private_network/output.tf
@@ -14,3 +14,6 @@ output "private_vpc_private_subnet_ids" {
     value = "${aws_subnet.ocp_pri_subnet.*.id}"
 }
 
+output "private_vpc_public_subnet_ids" {
+    value = "${aws_subnet.ocp_pub_subnet.*.id}"
+}
--- 1_private_network/variables.tf
+++ 1_private_network/variables.tf
@@ -28,3 +28,8 @@ variable "vpc_private_subnet_cidrs" {
   type        = "list"
   default     = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24" ]
 }
+variable "vpc_public_subnet_cidrs" {
+  description = "List of subnet CIDRs"
+  type        = "list"
+  default     = ["10.10.20.0/24", "10.10.21.0/24", "10.10.22.0/24" ]
+}
