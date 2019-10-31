8a30932b643d194ff4c6e30f08472d7b96b82c70
Budi Darmawan <vbudi@us.ibm.com>
2019-10-30 22:23:26 -0500

    merge 3 public

--- 1_private_network/subnet.tf
+++ 1_private_network/subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "ocp_pri_subnet" {
   count                   = "${length(var.aws_azs)}"
 
   vpc_id                  = "${aws_vpc.ocp_vpc.id}"
-  cidr_block              = "${element(var.public_vpc_private_subnet_cidrs, count.index)}"
+  cidr_block              = "${element(var.vpc_private_subnet_cidrs, count.index)}"
   availability_zone       = "${format("%s%s", element(list(var.aws_region), count.index), element(var.aws_azs, count.index))}"
 
   tags = "${merge(
@@ -106,7 +106,7 @@ resource "aws_subnet" "ocp_pub_subnet" {
   count                   = "${length(var.aws_azs)}"
 
   vpc_id                  = "${aws_vpc.ocp_vpc.id}"
-  cidr_block              = "${element(var.public_vpc_public_subnet_cidrs, count.index)}"
+  cidr_block              = "${element(var.vpc_public_subnet_cidrs, count.index)}"
   availability_zone       = "${format("%s%s", element(list(var.aws_region), count.index), element(var.aws_azs, count.index))}"
 
   tags = "${merge(
