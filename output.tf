2714b74defd89eb242494c9d9bcd0385a2c3ee4c
Budi Darmawan <vbudi@us.ibm.com>
2019-10-31 11:31:20 -0500

    adding output

---
+++ output.tf
@@ -0,0 +1,16 @@
+
+output "clustername" {
+    value = "${var.clustername}"
+}
+
+output "infrastructure_id" {
+    value = "${local.infrastructure_id}"
+}
+
+output "ingress_yaml" {
+    value = "You should remove the default ingress controller and ingress service using \n oc delete ingresscontroller default -n openshift-ingress-operator\n oc delete service router-default -n openshift-ingress \n oc create -f ${path.root}/ingress_controller.yml \n oc create -f ${path.root}/ingress_service.yml"
+}
+
+output "kubeadmin_password" {
+    value = "${file(8_bootstrap/${local.infrastructure_id}/auth/kubeadmin_password)}"
+}
