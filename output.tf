e8717d146b0724ac4fc5236bede19aba38b8a915
Budi Darmawan <vbudi@us.ibm.com>
2019-11-01 10:32:41 -0500

    fixing external lb

--- output.tf
+++ output.tf
@@ -6,8 +6,3 @@ output "clustername" {
 output "infrastructure_id" {
     value = "${local.infrastructure_id}"
 }
-
-output "ingress_yaml" {
-    value = "You should remove the default ingress controller and ingress service using \n oc delete ingresscontroller default -n openshift-ingress-operator\n oc delete service router-default -n openshift-ingress \n oc create -f ${path.root}/ingress_controller.yml \n oc create -f ${path.root}/ingress_service.yml"
-}
-
