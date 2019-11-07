a8c860aa80a61f7ebe8dd8b91d79711fc3a5f0df
Budi Darmawan <vbudi@us.ibm.com>
2019-11-06 18:26:31 -0600

    update docs

--- variables.tf
+++ variables.tf
@@ -1,25 +1,21 @@
-variable "aws_region" { default = "" }
+variable "aws_region" { default = "us-east-2" }
 variable "aws_azs" { default = ["a", "b", "c"] }
 variable "default_tags" { default = {} }
-variable "infrastructure_id" { default = "" }
-variable "clustername" { default = "ocp4" }
+variable "infrastructure_id" {  }
+variable "clustername" { }
 variable "private_vpc_cidr" { default = "10.10.0.0/16" }
-variable "public_vpc_cidr" { default = "172.16.0.0/16" }
-variable "public_vpc_id" { default = "" }
-variable "domain" { default = "" }
-variable "vpc_private_subnet_cidrs" { default = "" }
-variable "vpc_public_subnet_cidrs" { default = "" }
-variable "ami" { default = "" }
-variable "aws_access_key_id" { default = "" }
-variable "aws_secret_access_key" { default = "" }
+variable "domain" { }
+variable "vpc_private_subnet_cidrs" { default = ["10.10.10.0/24","10.10.11.0/24","10.10.12.0/24"] }
+variable "vpc_public_subnet_cidrs" { default = ["10.10.20.0/24","10.10.21.0/24","10.10.22.0/24"] }
+variable "ami" { default = "ami-0bc59aaa7363b805d" }
+variable "aws_access_key_id" {  }
+variable "aws_secret_access_key" {  }
 variable "cluster_network_cidr" { default = "192.168.0.0/17" }
 variable "cluster_network_host_prefix" { default = "23" }
 variable "service_network_cidr" { default = "192.168.128.0/24" }
-variable "bootstrap" { default = {         type = "i3.xlarge"} }
-variable "control_plane" { default = {         count = "3",         type = "m4.xlarge",         disk = "120"} }
-variable "worker" { default = {         count = "3",         type = "m4.large",         disk = "120"} }
+variable "bootstrap" { default = { type = "i3.xlarge"} }
+variable "control_plane" { default = { count = "3", type = "m4.xlarge", disk = "120"} }
+variable "worker" {        default = { count = "3", type = "m4.large",  disk = "120"} }
 variable "openshift_pull_secret" { default = "./openshift_pull_secret.json" }
 variable "use_worker_machinesets" { default = true }
 variable "openshift_installer_url" { default = "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest" }
-variable "vpc_public_subnet_ids" { default = [] }
-variable "vpc_private_subnet_ids" { default = [] }
