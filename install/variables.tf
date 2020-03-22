a6de7d67c1fd5ac97990d86f10580e5f7f56861e
Budi Darmawan <vbudi@us.ibm.com>
2020-03-21 19:52:15 -0500

    Working v001

--- install/variables.tf
+++ install/variables.tf
@@ -13,6 +13,11 @@ variable "domain" {
   description = "The DNS domain for the cluster."
 }
 
+variable "ami" {
+  type        = string
+  description = "The AMI ID for the RHCOS nodes"
+}
+
 variable "cluster_network_cidr" {
   type        = string
   default     = "192.168.0.0/17"
@@ -32,6 +37,32 @@ variable "cluster_network_host_prefix" {
   type        = string
   default     = "23"
 }
+
+variable "aws_worker_instance_type" {
+  type = string
+  description = "Instance type for the worker node(s). Example: `m4.large`."
+}
+
+variable "aws_worker_root_volume_type" {
+  type        = string
+  description = "The type of volume for the root block device of worker nodes."
+}
+
+variable "aws_worker_root_volume_size" {
+  type        = string
+  description = "The size of the volume in gigabytes for the root block device of worker nodes."
+}
+
+variable "aws_worker_root_volume_iops" {
+  type = string
+
+  description = <<EOF
+The amount of provisioned IOPS for the root block device of worker nodes.
+Ignored if the volume type is not io1.
+EOF
+
+}
+
 
 variable "master_count" {
   type        = number
@@ -64,4 +95,14 @@ variable "aws_region" {
   type        = string
   description = "AWS region"
   default     = "us-east-1"
+}
+
+variable "aws_worker_availability_zones" {
+  type = list(string)
+  description = "The availability zones to provision for workers.  Worker instances are created by the machine-API operator, but this variable controls their supporting infrastructure (subnets, routing, etc.)."
+}
+
+variable "dns_public_id" {
+  type = string
+  description = "public route53 id"
 }
