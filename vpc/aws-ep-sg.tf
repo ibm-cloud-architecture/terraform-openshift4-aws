4a06c90277a18ced15c57b82bb16f15c74e3001b
Budi Darmawan <vbudi@yahoo.com>
2020-03-24 18:48:33 +0000

    fixed typos

--- vpc/aws-ep-sg.tf
+++ vpc/aws-ep-sg.tf
@@ -1,5 +1,4 @@
 resource "aws_security_group" "private_ec2_api" {
-  count = var.airgapped.enabled ? 1 : 0
   name =  "${var.cluster_id}-ec2-api"
   vpc_id =  aws_vpc.new_vpc[0].id
 
@@ -13,7 +12,6 @@ resource "aws_security_group" "private_ec2_api" {
 
 # allow anybody in the VPC to talk to ec2 through the private endpoint
 resource "aws_security_group_rule" "private_ec2_ingress" {
-  count = var.airgapped.enabled ? 1 : 0
   type        = "ingress"
 
   from_port   = 0
@@ -23,11 +21,10 @@ resource "aws_security_group_rule" "private_ec2_ingress" {
      var.cidr_block
   ]
 
-  security_group_id =  aws_security_group.private_ec2_api[0].id
+  security_group_id =  aws_security_group.private_ec2_api.id
 }
 
 resource "aws_security_group_rule" "private_ec2_api_egress" {
-  count = var.airgapped.enabled ? 1 : 0
   type        = "egress"
 
   from_port   = 0
@@ -37,7 +34,7 @@ resource "aws_security_group_rule" "private_ec2_api_egress" {
     "0.0.0.0/0"
   ]
 
-  security_group_id =  aws_security_group.private_ec2_api[0].id
+  security_group_id =  aws_security_group.private_ec2_api.id
 }
 
 resource "aws_security_group" "private_ecr_api" {
